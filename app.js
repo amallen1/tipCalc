33c058de4a40e4e000af1336f9c3b7a825b7d986
Aniya <amallen046@gmail.com>
2021-07-21 15:10:12 -0500

    updated reset btn

--- app.js
+++ app.js
@@ -1,70 +1,40 @@
-let billInput = document.getElementById("bill-input-el");
+let billInputEl = document.getElementById("bill-input-el");
 let customAmountEl = document.getElementById("custom-input");
 let numPplEl = document.getElementById("people-input-el");
 let tipEl = document.getElementById("tip");
 let totalEl = document.getElementById("total");
 let resetButtonEl = document.getElementById("reset");
-
 let buttons = Array.from(document.getElementsByClassName("percent-btn"));
 
 let bill = 0;
-let defaultTip = 15;
 let customTip = 0;
 let tipAmt = 0;
 let billAmt = 0;
-let array = [];
+let percent = 0;
 
-billInput.addEventListener("change", function () {
-  bill = billInput.value;
+billInputEl.addEventListener("change", function () {
+  bill = billInputEl.value;
 });
 
-//for each button, add event listener
-
-let percent = 0;
 buttons.map((button) => {
   button.addEventListener("click", (e) => {
-
-    switch (button.id) {
-      case "5":
-        percent = parseInt(button.id) / 100;
-        array.push(percent);
-        break;
-
-      case "10":
-        percent = parseInt(button.id) / 100;
-        array.push(percent);
-        break;
-
-      case "15":
-        percent = parseInt(button.id) / 100;
-        array.push(percent);
-        break;
-
-      case "25":
-        percent = parseInt(button.id) / 100;
-        array.push(percent);
-        break;
-
-      case "50":
-        percent = parseInt(button.id) / 100;
-        array.push(percent);
-        break;
-
-      default:
-        console.log("DEFAULT");
-    }
+    button.style.backgroundColor = "#26c0ab";
+    percent = parseInt(button.id) / 100;
   });
 });
 
 numPplEl.addEventListener("change", function () {
   let numPpl = parseInt(numPplEl.value);
-
-  tipAmt = (bill * array[0]) / numPpl;
+  tipAmt = (bill * percent) / numPpl;
   billAmt = bill / numPpl;
 
-  tipEl.innerText = "$" + tipAmt.toFixed(2);
-  totalEl.innerText = "$" + (tipAmt + billAmt).toFixed(2);
+  tipEl.innerText = `$${tipAmt.toFixed(2)}`;
+  totalEl.innerText = `$${(tipAmt + billAmt).toFixed(2)}`;
 });
 
-
-
+resetButtonEl.addEventListener("click", (e) => {
+  billInputEl.value = "";
+  numPplEl.value = "";
+  tipEl.innerText = "$0.00";
+  totalEl.innerText = "$0.00";
+});
