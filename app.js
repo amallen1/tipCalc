82d127b7cc02ccf25fc35d42cd6ac7017078d23f
Aniya <amallen046@gmail.com>
2021-07-21 17:10:09 -0500

    update

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-let billInputEl = document.getElementById("bill-input-el");
-let customAmountEl = document.getElementById("custom-input");
-let numPplEl = document.getElementById("people-input-el");
-let tipEl = document.getElementById("tip");
-let totalEl = document.getElementById("total");
-let resetButtonEl = document.getElementById("reset");
-let buttons = Array.from(document.getElementsByClassName("percent-btn"));
+const billInputEl = document.getElementById("bill-input-el");
+const customAmountEl = document.getElementById("custom-input");
+const numPplEl = document.getElementById("people-input-el");
+const tipEl = document.getElementById("tip");
+const totalEl = document.getElementById("total");
+const resetButtonEl = document.getElementById("reset");
+const buttons = Array.from(document.getElementsByClassName("percent-btn"));
 
 let bill = 0;
 let customTip = 0;
-let tipAmt = 0;
-let billAmt = 0;
 let percent = 0;
 
-billInputEl.addEventListener("change", function () {
+billInputEl.addEventListener("input", function () {
   bill = billInputEl.value;
+  calculate();
 });
 
-buttons.map((button) => {
+buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    button.style.backgroundColor = "#26c0ab";
+    buttons.forEach((e) => e.classList.remove("selected-btn"));
+    e.target.classList.add("selected-btn");
     percent = parseInt(button.id) / 100;
+
+    console.log(numPplEl.value);
+    if (bill !== 0) calculate();
   });
 });
 
-numPplEl.addEventListener("change", function () {
-  let numPpl = parseInt(numPplEl.value);
-  tipAmt = (bill * percent) / numPpl;
-  billAmt = bill / numPpl;
-
-  tipEl.innerText = `$${tipAmt.toFixed(2)}`;
-  totalEl.innerText = `$${(tipAmt + billAmt).toFixed(2)}`;
+numPplEl.addEventListener("input", () => {
+  calculate();
 });
 
+const calculate = () => {
+  resetButtonEl.classList.remove('unselected-reset');
+  let numPpl = parseInt(numPplEl.value);
+  if (numPpl > 0) {
+    let tipAmt = (bill * percent) / numPpl;
+    let billAmt = bill / numPpl;
+    tipEl.innerText = `$${tipAmt.toFixed(2)}`;
+    totalEl.innerText = `$${(tipAmt + billAmt).toFixed(2)}`;
+  }
+};
+
 resetButtonEl.addEventListener("click", (e) => {
+  resetButtonEl.classList.add('unselected-reset');
+  buttons.forEach((button) => {
+    button.classList.remove("selected-btn");
+  });
   billInputEl.value = "";
   numPplEl.value = "";
   tipEl.innerText = "$0.00";
