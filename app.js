5d8032322d04b7d5e6598bb94bc6c2875364f818
Aniya <amallen046@gmail.com>
2021-07-22 13:34:11 -0500

    finished features

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const billInputEl = document.getElementById("bill-input-el");
-const customAmountEl = document.getElementById("custom-input");
+const customTipEl = document.getElementById("custom-input");
 const numPplEl = document.getElementById("people-input-el");
 const tipEl = document.getElementById("tip");
 const totalEl = document.getElementById("total");
 const resetButtonEl = document.getElementById("reset");
+const numPplLabel = document.getElementById('ppl-label')
 const buttons = Array.from(document.getElementsByClassName("percent-btn"));
 
 let bill = 0;
-let customTip = 0;
 let percent = 0;
+let numPpl = 0;
 
 billInputEl.addEventListener("input", function () {
   bill = billInputEl.value;
@@ -17,23 +18,41 @@ billInputEl.addEventListener("input", function () {
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    
     buttons.forEach((e) => e.classList.remove("selected-btn"));
     e.target.classList.add("selected-btn");
     percent = parseInt(button.id) / 100;
 
-    console.log(numPplEl.value);
-    if (bill !== 0) calculate();
   });
 });
 
+customTipEl.addEventListener("input", () => {
+  buttons.forEach((button) => {
+    button.classList.remove("selected-btn");
+  });
+
+  if (customTipEl.value === "") return;
+
+  percent = parseInt(customTipEl.value) / 100;
+  if (parseInt(numPplEl.value) > 0) calculate();
+});
+
 numPplEl.addEventListener("input", () => {
-  calculate();
+  numPpl = parseInt(numPplEl.value);
+  if (numPpl === 0) {
+    numPplEl.classList.add("input-focus");
+    numPplLabel.classList.add('ppl-label');
+  } else if (numPpl > 0) {
+    numPplEl.classList.remove("input-focus");
+    numPplLabel.classList.remove('ppl-label');
+    calculate();
+  }
 });
 
 const calculate = () => {
-  resetButtonEl.classList.remove('unselected-reset');
-  let numPpl = parseInt(numPplEl.value);
-  if (numPpl > 0) {
+  resetButtonEl.classList.add("selected-reset");
+
+  if (numPpl > 0 && bill > 0) {
     let tipAmt = (bill * percent) / numPpl;
     let billAmt = bill / numPpl;
     tipEl.innerText = `$${tipAmt.toFixed(2)}`;
@@ -42,11 +61,14 @@ const calculate = () => {
 };
 
 resetButtonEl.addEventListener("click", (e) => {
-  resetButtonEl.classList.add('unselected-reset');
+  resetButtonEl.classList.remove("selected-reset");
+
   buttons.forEach((button) => {
     button.classList.remove("selected-btn");
   });
+  
   billInputEl.value = "";
+  customTipEl.value = "";
   numPplEl.value = "";
   tipEl.innerText = "$0.00";
   totalEl.innerText = "$0.00";
